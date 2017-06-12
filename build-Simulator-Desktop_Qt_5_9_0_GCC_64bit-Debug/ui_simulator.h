739219f5c2a64706d3ec86725525055b564bf580
Ayush Malviya <ayushmalviya27@gmail.com>
2017-06-12 14:46:10 +0530

    Updated UI

--- build-Simulator-Desktop_Qt_5_9_0_GCC_64bit-Debug/ui_simulator.h
+++ build-Simulator-Desktop_Qt_5_9_0_GCC_64bit-Debug/ui_simulator.h
@@ -20,6 +20,7 @@
 #include <QtWidgets/QOpenGLWidget>
 #include <QtWidgets/QPushButton>
 #include <QtWidgets/QRadioButton>
+#include <QtWidgets/QSlider>
 #include <QtWidgets/QStatusBar>
 #include <QtWidgets/QToolBar>
 #include <QtWidgets/QWidget>
@@ -37,6 +38,12 @@ public:
     QRadioButton *radioButton_2;
     QRadioButton *radioButton_3;
     QLabel *label;
+    QSlider *verticalSlider;
+    QSlider *verticalSlider_2;
+    QSlider *verticalSlider_3;
+    QLabel *label_2;
+    QLabel *label_3;
+    QLabel *label_4;
     QMenuBar *menuBar;
     QToolBar *mainToolBar;
     QStatusBar *statusBar;
@@ -57,6 +64,7 @@ public:
         openGLWidget = new QOpenGLWidget(centralWidget);
         openGLWidget->setObjectName(QStringLiteral("openGLWidget"));
         openGLWidget->setGeometry(QRect(120, 40, 441, 361));
+        openGLWidget->setAutoFillBackground(false);
         radioButton = new QRadioButton(centralWidget);
         radioButton->setObjectName(QStringLiteral("radioButton"));
         radioButton->setGeometry(QRect(0, 130, 112, 23));
@@ -69,6 +77,27 @@ public:
         label = new QLabel(centralWidget);
         label->setObjectName(QStringLiteral("label"));
         label->setGeometry(QRect(0, 100, 101, 31));
+        verticalSlider = new QSlider(centralWidget);
+        verticalSlider->setObjectName(QStringLiteral("verticalSlider"));
+        verticalSlider->setGeometry(QRect(600, 140, 16, 160));
+        verticalSlider->setOrientation(Qt::Vertical);
+        verticalSlider_2 = new QSlider(centralWidget);
+        verticalSlider_2->setObjectName(QStringLiteral("verticalSlider_2"));
+        verticalSlider_2->setGeometry(QRect(640, 140, 16, 160));
+        verticalSlider_2->setOrientation(Qt::Vertical);
+        verticalSlider_3 = new QSlider(centralWidget);
+        verticalSlider_3->setObjectName(QStringLiteral("verticalSlider_3"));
+        verticalSlider_3->setGeometry(QRect(680, 140, 16, 160));
+        verticalSlider_3->setOrientation(Qt::Vertical);
+        label_2 = new QLabel(centralWidget);
+        label_2->setObjectName(QStringLiteral("label_2"));
+        label_2->setGeometry(QRect(590, 310, 31, 17));
+        label_3 = new QLabel(centralWidget);
+        label_3->setObjectName(QStringLiteral("label_3"));
+        label_3->setGeometry(QRect(630, 310, 31, 17));
+        label_4 = new QLabel(centralWidget);
+        label_4->setObjectName(QStringLiteral("label_4"));
+        label_4->setGeometry(QRect(670, 310, 31, 17));
         Simulator->setCentralWidget(centralWidget);
         menuBar = new QMenuBar(Simulator);
         menuBar->setObjectName(QStringLiteral("menuBar"));
@@ -95,6 +124,9 @@ public:
         radioButton_2->setText(QApplication::translate("Simulator", "Pyramid", Q_NULLPTR));
         radioButton_3->setText(QApplication::translate("Simulator", "Sphere", Q_NULLPTR));
         label->setText(QApplication::translate("Simulator", "Choose Shape:", Q_NULLPTR));
+        label_2->setText(QApplication::translate("Simulator", "xRot", Q_NULLPTR));
+        label_3->setText(QApplication::translate("Simulator", "yRot", Q_NULLPTR));
+        label_4->setText(QApplication::translate("Simulator", "zRot", Q_NULLPTR));
     } // retranslateUi
 
 };
